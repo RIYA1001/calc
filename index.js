953e55e2eb074f432cff0759f2d5721167de8b3f
RIYA KUSHWAHA <125476961+RIYA1001@users.noreply.github.com>
2023-09-17 15:25:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function display(value){
+    document.getElementById('textarea').value += value
+}
+
+function calculate(){
+    var value = document.getElementById('textarea').value
+    var ans = eval(value)
+    document.getElementById('textarea').value = ans
+}
+
+function clr(){
+    document.getElementById('textarea').value= ''
+}
